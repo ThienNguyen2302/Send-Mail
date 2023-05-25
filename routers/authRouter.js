2baa19f9deba18513e4ce71fbafa7292eb522d48
ThienNguyen2302 <nguyenngocthien749@gmail.com>
2023-05-25 09:08:38 +0700

    send email feature

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -10,13 +10,23 @@ const nodemailer = require("nodemailer")
 
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
+let transporter = nodemailer.createTransport({
+  host: "gmail",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL, // generated ethereal user
+    pass: process.env.EMAILPASSWORD, // generated ethereal password
+  },
+});
+
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: "http://localhost:3000/auth/google/callback",
   passReqToCallback: true,
 },
-function(request, accessToken, refreshToken, profile, done) {
+async (request, accessToken, refreshToken, profile, done) => {
   console.log(profile.displayName)
   User.findOne({id: profile.id})
   .then(u => {
@@ -29,13 +39,25 @@ function(request, accessToken, refreshToken, profile, done) {
         email: profile.email
       })
       user.save()
-      const hash = bcrypt.hashSync(profile.id,10)
-      const activated = new Activated({
-        id: profile.id,
-        activate: false,
-        activateString: hash
+      .then(u => {
+        const hash = bcrypt.hashSync(profile.id,10)
+        const activated = new Activated({
+          id: profile.id,
+          activate: false,
+          activateString: hash
+        })
+        activated.save()
+        .then(a => {
+          // send mail with defined transport object
+          transporter.sendMail({
+            from: '"send Mail WEB" <tieuthien951@gmail.com>', // sender address
+            to: profile.email, // list of receivers
+            subject: "Activated Account", // Subject line
+            text: "Your account need to activation lick here to do it:", // plain text body
+            html: `<a href = http://localhost:3000/activated/${a.id}/${a.activateString}}>Hello world?</a>`, // html body
+          });
+        })
       })
-      activated.save()
       
     }
   })
